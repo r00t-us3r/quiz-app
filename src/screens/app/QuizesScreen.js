a54735f6a33811542f120822abc525ff7b81f81c
Dean Konkolewski <dkonkolewski@Deans-iMac.local>
2023-06-11 22:52:17 +0100

    fix drawer nav

--- src/screens/app/QuizesScreen.js
+++ src/screens/app/QuizesScreen.js
@@ -1,6 +1,6 @@
-import {Text, View} from "react-native";
+import {Button, Text, View} from "react-native";
 
-export const QuizesScreen = () => {
+export const QuizesScreen = ({navigation}) => {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Quizes Screen</Text>
@@ -14,7 +14,7 @@ export const QuizesScreen = () => {
             <Text>Quizes Screen</Text>
             <Text>Quizes Screen</Text>
             <Text>Quizes Screen</Text>
-            <Text>Quizes Screen</Text>
+            <Button onPress={() => navigation.openDrawer()} title="open launcher" />
         </View>
     )
 }
